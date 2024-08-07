2bd1eeceb8277eafc5ce608a042b06d28d3718f6
Surma <surma@surma.dev>
2024-08-07 10:34:05 +0100

    Work on all systems

--- codecs/mozjpeg/flake.nix
+++ codecs/mozjpeg/flake.nix
@@ -1,9 +1,8 @@
 {
   inputs = {
-    nixpkgs = {
-      url = "github:nixos/nixpkgs/24.05";
-    };
-    mozjpeg = {
+    nixpkgs.url = "github:nixos/nixpkgs/24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    mozjpeg-src = {
       url = "github:mozilla/mozjpeg/v3.3.1";
       flake = false;
     };
@@ -12,80 +11,83 @@
     {
       self,
       nixpkgs,
-      mozjpeg,
+      flake-utils,
+      mozjpeg-src,
     }:
-    let
-      system = "aarch64-darwin";
-      pkgs = import nixpkgs { inherit system; };
-    in
-    with pkgs;
-    rec {
-      packages.${system} = {
-        default = stdenv.mkDerivation {
-          name = "mozjpeg-squoosh";
-          src = ./.;
-          nativeBuildInputs = [
-            emscripten
-            packages.${system}.mozjpeg
-          ];
-          MOZJPEG = packages.${system}.mozjpeg;
-          dontConfigure = true;
-          buildPhase = ''
-            export HOME=$TMPDIR
-            emmake make -j$(nproc)
-          '';
-          installPhase = ''
-            mkdir -p $out
-            cp enc/*.{wasm,js} $out
-          '';
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      with pkgs;
+      {
+        packages = rec {
+          default = stdenv.mkDerivation {
+            name = "mozjpeg-squoosh";
+            src = ./.;
+            nativeBuildInputs = [
+              emscripten
+              mozjpeg
+            ];
+            MOZJPEG = mozjpeg;
+            dontConfigure = true;
+            buildPhase = ''
+              export HOME=$TMPDIR
+              emmake make -j$(nproc)
+            '';
+            installPhase = ''
+              mkdir -p $out
+              cp enc/*.{wasm,js} $out
+            '';
+          };
+          mozjpeg = stdenv.mkDerivation {
+            name = "mozjpeg";
+            src = mozjpeg-src;
+            nativeBuildInputs = [
+              autoconf
+              automake
+              libtool
+              emscripten
+              pkg-config
+            ];
+            configurePhase = ''
+                # $HOME is required for Emscripten to work.
+                # See: https://nixos.org/manual/nixpkgs/stable/#emscripten
+              	export HOME=$TMPDIR
+              	autoreconf -ifv
+                emconfigure ./configure \
+                  --disable-shared \
+                  --without-turbojpeg \
+                  --without-simd \
+                  --without-arith-enc \
+                  --without-arith-dec \
+                  --with-build-date=squoosh \
+                  --prefix=$out
+            '';
+            buildPhase = ''
+              export HOME=$TMPDIR
+              emmake make V=1 -j$(nproc) --trace 
+            '';
+            installPhase = ''
+              make install
+              cp *.h $out/include
+              cp rdswitch.o $out/lib
+            '';
+            dontFixup = true;
+          };
         };
-        mozjpeg = stdenv.mkDerivation {
-          name = "mozjpeg";
-          src = mozjpeg;
-          nativeBuildInputs = [
+        devShells.default = pkgs.mkShell {
+          packages = [
             autoconf
             automake
             libtool
             emscripten
             pkg-config
           ];
-          configurePhase = ''
-              # $HOME is required for Emscripten to work.
-              # See: https://nixos.org/manual/nixpkgs/stable/#emscripten
-            	export HOME=$TMPDIR
-            	autoreconf -ifv
-              emconfigure ./configure \
-                --disable-shared \
-                --without-turbojpeg \
-                --without-simd \
-                --without-arith-enc \
-                --without-arith-dec \
-                --with-build-date=squoosh \
-                --prefix=$out
+          shellHook = ''
+            echo "Path to MozJPEG: ${mozjpeg-src}"
           '';
-          buildPhase = ''
-            export HOME=$TMPDIR
-            emmake make V=1 -j$(nproc) --trace 
-          '';
-          installPhase = ''
-            make install
-            cp *.h $out/include
-            cp rdswitch.o $out/lib
-          '';
-          dontFixup = true;
         };
-      };
-      devShells.${system}.default = pkgs.mkShell {
-        packages = [
-          autoconf
-          automake
-          libtool
-          emscripten
-          pkg-config
-        ];
-        shellHook = ''
-          echo "Path to MozJPEG: ${mozjpeg}"
-        '';
-      };
-    };
+      }
+    );
 }
