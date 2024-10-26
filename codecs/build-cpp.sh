8a7de2abcf589366c97b732ab3ce0dc4a33d9f08
Michael Nutt <michael@nuttnet.net>
2024-10-26 10:15:35 -0400

    upgrade to latest emsdk

--- codecs/build-cpp.sh
+++ codecs/build-cpp.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -e
-docker build -t squoosh-cpp - < ../cpp.Dockerfile
-docker run -it --rm -v $PWD:/src squoosh-cpp "$@"
+docker build -t squoosh-cpp --platform=linux/amd64 - < ../cpp.Dockerfile
+docker run -it --rm --platform=linux/amd64 -v $PWD:/src squoosh-cpp "$@"
